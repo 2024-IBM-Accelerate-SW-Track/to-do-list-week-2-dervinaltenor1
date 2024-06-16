330c561eba8ef366485913786c4154304953dc82
dervinaltenor1 <156249938+dervinaltenor1@users.noreply.github.com>
2024-06-16 14:04:33 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,14 @@ class Home extends Component {
       todos: [],
     };
   }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+  };
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -20,11 +28,17 @@ class Home extends Component {
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+    const exists = this.state.todos.find(t => t === todo.id)
+    if (exists) {
+      return
+    }
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+      // Update the local state with the new array.
+
     this.setState({
       todos: new_list,
+
     });
   };
   render() {
@@ -36,7 +50,8 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+
       </div>
     );
   }
